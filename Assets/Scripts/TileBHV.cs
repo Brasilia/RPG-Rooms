12ba55ed744fcad1ee117e780cc08e7ac998321a
brasilia <ksuspec@gmail.com>
2018-04-03 21:20:40 -0300

    Add no script e prefab dos Tiles que estavam faltando

---
+++ Assets/Scripts/TileBHV.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBHV : MonoBehaviour {
+
+	public int x;
+	public int y;
+	public int id;
+
+	// Use this for initialization
+	void Start () {
+		if (id == 1){ // obstáculo
+			GetComponent<Collider2D> ().enabled = true; // ativa o colisor
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetPosition(int x, int y, float centerX, float centerY){
+		this.x = x;
+		this.y = y;
+		transform.localPosition = new Vector2 (centerX, centerY);
+	}
+}
