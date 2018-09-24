34176e5237612f2cf936e712627dba2252bd0e87
brasilia <ksuspec@gmail.com>
2018-09-23 22:52:50 -0300

    Adicionado form

---
+++ Assets/Scripts/FormQuestionBHV.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class FormQuestionBHV : MonoBehaviour {
+
+    public int value;
+    public Toggle[] toggles;
+    public Text questionText;
+    public Text descriptionText;
+
+    private FormQuestionData questionData;
+
+
+
+    void Awake()
+    {
+        toggles = GetComponentsInChildren<Toggle>().ToArray<Toggle>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeValue(Toggle selected)
+    {
+        if (!selected.isOn)
+        {
+            return;
+        }
+        value = int.Parse(selected.GetComponentInChildren<Text>().text);
+        if (selected.isOn)
+        {
+            foreach (Toggle t in toggles)
+            {
+                if (t != selected)
+                {
+                    t.isOn = false;
+                }
+            }
+        }
+    }
+
+    public void LoadData(FormQuestionData q)
+    {
+        questionData = q;
+        questionText.text = q.question;
+        descriptionText.text = q.description;
+    }
+
+}
